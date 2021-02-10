f102248cafb75cf7ab76e9f64265d3c8090d0cf6
yymin1022 <yymin1022@gmail.com>
2021-02-10 03:39:28 +0000

    Update Project Structure

---
+++ react/src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Main.css';
+
+import PostBox from './PostBox';
+
+function Main() {
+  return (
+    <div className="Main">
+      <PostBox />
+    </div>
+  );
+}
+
+export default Main;
