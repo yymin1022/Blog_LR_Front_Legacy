90e995cea78407704e80c81b64b9129fab391394
yymin1022 <yymin1022@gmail.com>
2021-02-10 02:12:33 +0000

    Update Files

---
+++ flask-server/static/react/precache-manifest.761105306ad057fe684f8cd91385f95b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7108268735dd93ec8d983d42206520a2",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "343a93e89db126503909",
+    "url": "/static/react/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "a9834cc86ba6db6db09f",
+    "url": "/static/react/static/js/2.12607903.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/react/static/js/2.12607903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "343a93e89db126503909",
+    "url": "/static/react/static/js/main.2b605dc1.chunk.js"
+  },
+  {
+    "revision": "00b90c6979e7b9785281",
+    "url": "/static/react/static/js/runtime-main.3a44c33a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
