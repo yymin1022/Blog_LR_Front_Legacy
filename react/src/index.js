0bcb92c9907bbc7a68f01c8c929395c549f0199e
yymin1022 <yymin1022@gmail.com>
2021-02-10 03:40:17 +0000

    Update Project Structure

--- react/src/index.js
+++ react/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Main from './Main';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('root'));
