f35b678277e58a48de02fd8b6cd6599890cd49bb
yymin1022 <yymin1022@gmail.com>
2021-02-10 03:34:16 +0000

    Update PostBox.js

---
+++ react/src/PostBox.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class PostBox extends Component {
+
+    render(){
+        return(
+            <div>
+                Hello, World!
+                PostBox!
+            </div>
+        );
+    }
+}
+
+export default PostBox;
