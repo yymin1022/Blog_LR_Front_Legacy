1e81de8d4eb49e5607be6f1fbccb7c5e77e0a5ad
yymin1022 <yymin1022@gmail.com>
2021-02-10 03:52:48 +0000

    Improve Material Library from CSS to SASS

---
+++ react/src/HeaderBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class HeaderBar extends Component {
+
+    render() {
+        return (
+            <div>
+                <a>GOING TO BE HEADER</a>
+            </div>
+        );
+    }
+}
+
+export default HeaderBar;
--- react/src/Main.js
+++ react/src/Main.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './Main.css';
 
+import './materialize.css';
+
+import HeaderBar from './HeaderBar';
 import PostBox from './PostBox';
 
 function Main() {
   return (
-    <div className="Main">
-      <PostBox />
+    <div>
+      <nav>
+        <div className="nav-wrapper">
+          <HeaderBar />
+        </div>
+      </nav>
+
+      <div className="Main">
+        <PostBox />
+      </div>
     </div>
   );
 }
