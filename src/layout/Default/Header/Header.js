8490b3cd79ca2ec3c1c60e0534a7d211c6ef27dc
yymin1022 <yymin1022@gmail.com>
2021-03-12 17:37:38 +0000

    Add AppBar on Header

--- src/layout/Default/Header/Header.js
+++ src/layout/Default/Header/Header.js
@@ -3,6 +3,14 @@ import PropTypes from 'prop-types';
 
 import styled from 'styled-components';
 
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
 const Container = styled.div`
   position: fixed;
   top: 0;
@@ -13,6 +21,18 @@ const Container = styled.div`
   background-color: skyblue;
 `;
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 const Header = props => {
   const {
     className,
@@ -20,7 +40,19 @@ const Header = props => {
   
   return (
     <Container className={ className }>
-      Header
+      <div className={useStyles().root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={useStyles().menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={useStyles().title}>
+              LR's Blog
+            </Typography>
+            <Button color="inherit">about</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
     </Container>
   );
 }
