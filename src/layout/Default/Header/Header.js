d48208384c4c185c8ad27b5ca3145b7920d6613d
yymin1022 <yymin1022@gmail.com>
2021-03-15 11:47:08 +0000

    Update Header : Fix Directory related Error

--- src/layout/Default/Header/Header.js
+++ src/layout/Default/Header/Header.js
@@ -11,7 +11,7 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
-import {toggleDrawer} from './Default'
+import {toggleDrawer} from '../Default'
 
 const Container = styled.div`
   position: fixed;
