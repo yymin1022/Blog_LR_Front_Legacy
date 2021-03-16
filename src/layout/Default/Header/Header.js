a9c120b86dcc0a2b619c27023ef269b1d1d6fe2e
yymin1022 <yymin1022@gmail.com>
2021-03-16 11:55:04 +0000

    Re-Orginized Imports, Cleaned up Codes

--- src/layout/Default/Header/Header.js
+++ src/layout/Default/Header/Header.js
@@ -10,7 +10,7 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
 import styled from 'styled-components';
-import }makeStyles} from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 
 const Container = styled.div`
   position: fixed;
