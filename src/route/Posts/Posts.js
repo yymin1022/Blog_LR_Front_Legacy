13d286dfad8a0caeb4bc74c64f225fd82432bb47
yymin1022 <yymin1022@gmail.com>
2021-03-17 07:34:03 +0000

    Improve Post Page Parameter

--- src/route/Posts/Posts.js
+++ src/route/Posts/Posts.js
@@ -6,16 +6,15 @@ import styled from 'styled-components';
 const Container = styled.div`
 `;
 
-const {category} = props.location;
-
 const Posts = props => {
   const {
     className,
+    location,
   } = props;
 
   return (
     <Container className={className}>
-      <div>Posts(Category is {category})</div>
+      <div>Posts(Category is {location})</div>
     </Container>
   );
 }
