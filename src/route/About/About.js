03f69f0c335a37af246cffd4d68c06ddf51637b7
yymin1022 <yymin1022@gmail.com>
2021-03-20 12:26:00 +0900

    Update About : Removed Testing Codes

--- src/route/About/About.js
+++ src/route/About/About.js
@@ -1,20 +1,15 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 
 import AppMarkdown from '/home/server/web/src/posts/about.md';
 import ReactMarkdown from 'react-markdown';
 
-import styled from 'styled-components';
-
 class About extends Component {
-
   constructor() {
     super();
     this.state = { markdown: '' };
   }
 
   componentWillMount() {
-    // Get the contents from the Markdown file and put them in the React state, so we can reference it in render() below.
     fetch(AppMarkdown).then(res => res.text()).then(text => this.setState({ markdown: text }));
   }
 
