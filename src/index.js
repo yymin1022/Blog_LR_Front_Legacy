2732b9713ea9fdec01af0e4f24b6a4e165799866
yymin1022 <yymin1022@gmail.com>
2021-03-20 13:11:48 +0900

    Update NotoSans Font Information

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
+import './index.css';
+
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: 'Roboto',
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
