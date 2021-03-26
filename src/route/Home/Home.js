c8e6733842a8fd2925a37ab75de3019709c88814
yymin1022 <yymin1022@gmail.com>
2021-03-26 21:10:33 +0900

    Add Debugging Code for Post Loading

--- src/route/Home/Home.js
+++ src/route/Home/Home.js
@@ -1,30 +1,46 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import styled from 'styled-components';
 
-const Container = styled.div`
-`;
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-const Home = props => {
-  const {
-    className,
-  } = props;
+  render() {
+    function getPosts(props){
+      var fs = require('fs');
+      fs.readdir('/posts', (err, file_list) => {
+        alert(file_list);
+      });
+    }
 
-  return (
-    <Container className={className}>
-      for(var i = 0; i < 1; i++){
-        var fs = require('fs');
-        fs.readdir('/posts', (err, file_list) => {
-          alert(file_list);
-        }
-      }
-    </Container>
-  );
+    return(
+      <div>
+        {getPosts};
+      </div>
+    )
+  }
 }
 
-Home.propTypes = {
-  className: PropTypes.string,
-}
+// const Container = styled.div`
+// `;
+
+// const Home = props => {
+//   const {
+//     className,
+//   } = props;
+
+//   return (
+//     <Container className={className}>
+      
+//     </Container>
+//   );
+// }
+
+// Home.propTypes = {
+//   className: PropTypes.string,
+// }
 
 export default Home;
