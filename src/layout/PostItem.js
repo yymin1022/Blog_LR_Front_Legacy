5ade5bd29f011bd283f74cfcb007ef9e874212cb
yymin1022 <yymin1022@gmail.com>
2021-03-29 10:40:12 +0900

    Add CSS on PostItem Component

--- src/layout/PostItem.js
+++ src/layout/PostItem.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 
+import './PostItem.css';
+
 class PostItem extends Component {
   constructor() {
     super();
--- src/layout/PostItemPinned.js
+++ src/layout/PostItemPinned.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 
+import './PostItemPinned.css';
+
 class PostItemPinned extends Component {
   constructor() {
     super();
