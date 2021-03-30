1cc2867ed9c0eb2260b8993fbada3d23e8d61f71
yymin1022 <yymin1022@gmail.com>
2021-03-30 18:40:17 +0900

    Update PostItem Component Layout

--- src/route/Home/Home.js
+++ src/route/Home/Home.js
@@ -19,7 +19,7 @@ class Home extends Component {
           {
             postDB.map(item => {
               return(
-                <div>
+                <div style="margin-bottom: -10px;">
                   <If condition={item.pinned}>
                     <Then>
                       <PostItemPinned
