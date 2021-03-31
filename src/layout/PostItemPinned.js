22a1ab5fc55cb02df268202422be58feab169d75
yymin1022 <yymin1022@gmail.com>
2021-03-31 10:38:59 +0900

    Update PostItemPinned Layout

--- src/layout/PostItemPinned.js
+++ src/layout/PostItemPinned.js
@@ -12,6 +12,32 @@ class PostItemPinned extends Component {
     return(
       <div>
         <a href={this.props.url1}>
+          <div id="postItem">
+            <div id="postThumbnail">
+              <img src={require("/home/server/web/src/posts/" + this.props.thumbnail1 + ".png").default} />
+            </div>
+            <div id="postInfo">
+              <div id="postInfoTitle">
+                <p>
+                !!Pinned!! {this.props.title1}
+                </p>
+              </div>
+              <div id="postInfoTag">
+                <p>
+                  #{this.props.tag1}
+                </p>
+              </div>
+              <div id="postInfoDate">
+                <p>
+                  {this.props.date1}
+                </p>
+              </div>
+            </div>
+          </div>
+        </a>
+        <br/>
+
+        {/* <a href={this.props.url1}>
           <div id="postItem" align="left">
             <div id="postThumbnail">
               <img src={require("/home/server/web/src/posts/" + this.props.thumbnail1 + ".png").default} />
@@ -84,7 +110,7 @@ class PostItemPinned extends Component {
               </div>
             </div>
           </div>
-        </a>
+        </a> */}
       </div>
     )
   }
