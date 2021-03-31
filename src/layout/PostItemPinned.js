832dcf4369ddee781b16f36706b932076d644ef0
yymin1022 <yymin1022@gmail.com>
2021-03-31 10:46:30 +0900

    Update PostItemPinned Layout

--- src/layout/PostItemPinned.js
+++ src/layout/PostItemPinned.js
@@ -12,22 +12,22 @@ class PostItemPinned extends Component {
     return(
       <div id="postPinned">
         <a href={this.props.url1}>
-          <div id="postItem" align="left">
-            <div id="postThumbnail">
+          <div id="postPinnedItem" align="left">
+            <div id="postPinnedThumbnail">
               <img src={require("/home/server/web/src/posts/" + this.props.thumbnail1 + ".png").default} />
             </div>
-            <div id="postInfo">
-              <div id="postInfoTitle">
+            <div id="postPinnedInfo">
+              <div id="postPinnedInfoTitle">
                 <p>
                 !!Pinned!! {this.props.title1}
                 </p>
               </div>
-              <div id="postInfoTag">
+              <div id="postPinnedInfoTag">
                 <p>
                   #{this.props.tag1}
                 </p>
               </div>
-              <div id="postInfoDate">
+              <div id="postPinnedInfoDate">
                 <p>
                   {this.props.date1}
                 </p>
@@ -37,22 +37,22 @@ class PostItemPinned extends Component {
         </a>
         <br/>
         <a href={this.props.url2}>
-          <div id="postItem" align="left">
-            <div id="postThumbnail">
+          <div id="postPinnedItem" align="left">
+            <div id="postPinnedThumbnail">
               <img src={require("/home/server/web/src/posts/" + this.props.thumbnail2 + ".png").default} />
             </div>
-            <div id="postInfo">
-              <div id="postInfoTitle">
+            <div id="postPinnedInfo">
+              <div id="postPinnedInfoTitle">
                 <p>
                 !!Pinned!! {this.props.title2}
                 </p>
               </div>
-              <div id="postInfoTag">
+              <div id="postPinnedInfoTag">
                 <p>
                   #{this.props.tag2}
                 </p>
               </div>
-              <div id="postInfoDate">
+              <div id="postPinnedInfoDate">
                 <p>
                   {this.props.date2}
                 </p>
@@ -62,22 +62,22 @@ class PostItemPinned extends Component {
         </a>
         <br/>
         <a href={this.props.url3}>
-          <div id="postItem" align="left">
-            <div id="postThumbnail">
+          <div id="postPinnedItem" align="left">
+            <div id="postPinnedThumbnail">
               <img src={require("/home/server/web/src/posts/" + this.props.thumbnail3 + ".png").default} />
             </div>
-            <div id="postInfo">
-              <div id="postInfoTitle">
+            <div id="postPinnedInfo">
+              <div id="postPinnedInfoTitle">
                 <p>
                 !!Pinned!! {this.props.title3}
                 </p>
               </div>
-              <div id="postInfoTag">
+              <div id="postPinnedInfoTag">
                 <p>
                   #{this.props.tag3}
                 </p>
               </div>
-              <div id="postInfoDate">
+              <div id="postPinnedInfoDate">
                 <p>
                   {this.props.date3}
                 </p>
