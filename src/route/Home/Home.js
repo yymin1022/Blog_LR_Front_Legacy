91a16749e83efb8030c69c47176e007e9f7f0a3f
yymin1022 <yymin1022@gmail.com>
2021-03-31 10:50:27 +0900

    Update PostItemPinned Layout

--- src/route/Home/Home.js
+++ src/route/Home/Home.js
@@ -22,37 +22,40 @@ class Home extends Component {
             </a>
           </div>
 
-          {
-            postDB.map(item => {
-              return(
-                <div>
-                  <br/>
-                  <If condition={item.pinned}>
-                    <Then>
-                      <PostItemPinned
-                        date1 = {item.date1}
-                        tag1 = {item.tag1}
-                        thumbnail1 = {item.thumbnail1}
-                        title1 = {item.title1}
-                        url1 = {item.url1}
-                        
-                        date2 = {item.date2}
-                        tag2 = {item.tag2}
-                        thumbnail2 = {item.thumbnail2}
-                        title2 = {item.title2}
-                        url2 = {item.url2}
-                        
-                        date3 = {item.date3}
-                        tag3 = {item.tag3}
-                        thumbnail3 = {item.thumbnail3}
-                        title3 = {item.title3}
-                        url3 = {item.url3} />
-                    </Then>
-                  </If>
-                </div>
-              );
-            })
-          }
+          <div style={{width: "85%"}}>
+            {
+              postDB.map(item => {
+                return(
+                  <div>
+                    <br/>
+                    <If condition={item.pinned}>
+                      <Then>
+                        <PostItemPinned
+                          date1 = {item.date1}
+                          tag1 = {item.tag1}
+                          thumbnail1 = {item.thumbnail1}
+                          title1 = {item.title1}
+                          url1 = {item.url1}
+                          
+                          date2 = {item.date2}
+                          tag2 = {item.tag2}
+                          thumbnail2 = {item.thumbnail2}
+                          title2 = {item.title2}
+                          url2 = {item.url2}
+                          
+                          date3 = {item.date3}
+                          tag3 = {item.tag3}
+                          thumbnail3 = {item.thumbnail3}
+                          title3 = {item.title3}
+                          url3 = {item.url3} />
+                      </Then>
+                    </If>
+                  </div>
+                );
+              })
+            }
+          </div>
+          
         </div>
         <hr style={{width: "85%"}}/>
         <br/>
