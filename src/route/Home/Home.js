2864c6609004f62680ebd80058f15972bdf30c11
yymin1022 <yymin1022@gmail.com>
2021-04-03 22:24:17 +0900

    Resize FontAwesome Icon on Pinned Post

--- src/route/Home/Home.js
+++ src/route/Home/Home.js
@@ -35,7 +35,7 @@ class Home extends Component {
                       <div>
                         <If condition={item.pinned}>
                           <Then>
-                            <FontAwesomeIcon icon={faThumbtack} size="2x"/>
+                            <FontAwesomeIcon icon={faThumbtack}/>
                             <a style={{textSize: "25px"}}>
                               Pinned
                             </a>
