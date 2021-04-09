04efd929cc247f01327f24f6e5adc9a264d6fef0
yymin1022 <yymin1022@gmail.com>
2021-04-09 12:25:33 +0900

    Update CodeBlock.js : Update Prism Highlighter Theme

--- src/route/PostView/CodeBlock.js
+++ src/route/PostView/CodeBlock.js
@@ -1,7 +1,7 @@
 import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import {Prism as SyntaxHighlighter} from "react-syntax-highlighter";
-import {coy} from "react-syntax-highlighter/dist/esm/styles/prism";
+import {materialDark} from "react-syntax-highlighter/dist/esm/styles/prism";
 
 class CodeBlock extends PureComponent {
     static propTypes = {
@@ -16,7 +16,7 @@ class CodeBlock extends PureComponent {
     render() {
         const {language, value} = this.props;
         return(
-            <SyntaxHighlighter language={language} style={coy}>
+            <SyntaxHighlighter language={language} style={materialDark}>
                 {value}
             </SyntaxHighlighter>
         );
