4421178022a14912e565885153812d08b65edcb9
yymin1022 <yymin1022@gmail.com>
2021-04-09 13:41:47 +0900

    Update Header.js : Improved Reoute Algorithm

--- src/layout/Default/Header/Header.js
+++ src/layout/Default/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
