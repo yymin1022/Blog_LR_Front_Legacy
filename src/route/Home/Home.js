c1480b5bb691b945d008e9714cea240e2476f373
yymin1022 <yymin1022@gmail.com>
2021-04-13 20:44:17 +0900

    Update Home.js : Add Code for Debugging

--- src/route/Home/Home.js
+++ src/route/Home/Home.js
@@ -22,7 +22,7 @@ class Home extends Component {
       this.categoryID = this.props.match.params.categoryID;
     }
     
-    alert(this.categoryID);
+    console.log(this.props.match.params);
   }
 
   render() {
