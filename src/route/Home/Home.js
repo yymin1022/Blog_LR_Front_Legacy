195230409a5228836cdd34c7d631f62b8181f1c5
yymin1022 <yymin1022@gmail.com>
2021-04-14 17:15:23 +0900

    Update Home.js : Improve Route Structure

--- src/route/Home/Home.js
+++ src/route/Home/Home.js
@@ -24,7 +24,7 @@ class Home extends Component {
   }
 
   componentDidUpdate(prevProps) {
-    if (prevProps.location.state.categoryID !== this.props.location.state.ategoryID){
+    if (prevProps.location.state.categoryID !== this.props.location.state.categoryID){
       window.location.reload();
     }
   }
