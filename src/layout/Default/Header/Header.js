895b97be6f4c3e08df53f5fc0e3a60c9d06103ac
yymin1022 <yymin1022@gmail.com>
2021-04-14 16:00:59 +0900

    Update Header.js : Route Change /home to /

--- src/layout/Default/Header/Header.js
+++ src/layout/Default/Header/Header.js
@@ -53,7 +53,7 @@ const Header = props => {
             <Typography variant="h6" className={useStyles().title}>
               LR의 IT블로그
             </Typography>
-            <Link to={"/home"}>
+            <Link to={"/"}>
               <Button style={{color: "#FFFFFF"}}>Home</Button>
             </Link>
             <Link to={"/about"}>
