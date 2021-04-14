722bfb301dcd4b2962eed481ee1d770eecba9ae3
yymin1022 <yymin1022@gmail.com>
2021-04-14 15:44:11 +0900

    Update files : Debugging Code

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           <Redirect
             exact
             from = "/"
-            to = {"/home/"+ null} />
+            to = {"/home/"+ " "} />
           <RouteLayout
             path = "/about"
             layout = {DefaultLayout}
