93e612ec776d269c905668e5873c291f0c15d4d8
yymin1022 <yymin1022@gmail.com>
2021-04-14 17:36:02 +0900

    Update Home.js : Remove Div Key related Warning

--- src/route/Home/Home.js
+++ src/route/Home/Home.js
@@ -47,7 +47,7 @@ class Home extends Component {
                 {
                   postDB.map(item => {
                     return(
-                      <div key={item.postURL + "-pinned"}>
+                      <div key={item.postThumbnail}>
                         <If condition={item.pinned}>
                           <Then>
                             <If condition={categoryID == "all"}>
