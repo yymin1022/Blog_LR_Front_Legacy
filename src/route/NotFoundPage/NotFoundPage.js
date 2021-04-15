781076b2acc1b8e5c5fc60e6c43cc423cd5b05c9
yymin1022 <yymin1022@gmail.com>
2021-04-15 18:20:31 +0900

    Update NotFoundPage.js : Automatically Redirect to Home

--- src/route/NotFoundPage/NotFoundPage.js
+++ src/route/NotFoundPage/NotFoundPage.js
@@ -1,23 +1,40 @@
-import React, {Component} from "react";
+import React,{Component} from "react";
+import{Redirect} from 'react-router-dom';
 
-import "./NotFoundPage.css";
 import DefConLogo from '/home/server/web/src/image/DefCon_Logo.svg';
+import "./NotFoundPage.css";
 
-class NotFoundPage extends Component {
-  constructor() {
-    super();
+class NotFoundPage extends Component{
+  constructor(props){
+    super(props);
+    this.state ={
+      doRedirect: false
+    }
   }
 
-  render() {
-    return(
-      <div id="Container">
-        <div id="Content" align="center">
-          <img id="DefConLogo" src={DefConLogo} />
+  componentDidMount(){
+    setTimeout(function(){
+      this.setState({doRedirect: true})
+    }.bind(this), 2000)
+  }
 
-          <h1>Page Not Found !</h1>
+  render(){
+    if(this.state.doRedirect){
+        return(
+          <Redirect to="/" />
+        )
+    }else{
+      return(
+        <div id="Container">
+          <div id="Content" align="center">
+            <img id="DefConLogo" src={DefConLogo} />
+  
+            <h1>Page Not Found !</h1>
+            <h3>Redirecting to Blog Home...</h3>
+          </div>
         </div>
-      </div>
-    )
+      )
+    }
   }
 }
 
