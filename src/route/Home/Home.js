67cd91be2f381cfdfefb506f4584fb67a66da812
yymin1022 <yymin1022@gmail.com>
2021-04-15 18:43:23 +0900

    Update Home.js : Improve Rendering Structure

--- src/route/Home/Home.js
+++ src/route/Home/Home.js
@@ -13,7 +13,7 @@ import "./Home.css";
 class Home extends Component {
   constructor(props) {
     super();
-    this.state = {};
+    this.state = {doUpdate: ""};
     this.categoryID = "all";
   }
 
@@ -24,7 +24,7 @@ class Home extends Component {
   }
 
   componentDidUpdate() {
-    window.location.reload();
+    this.setState({doUpdate: new Date().getTime()})
   }
 
   render() {
