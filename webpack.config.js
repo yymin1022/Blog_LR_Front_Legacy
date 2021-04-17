9da51fac767090f71b462e6d14c39e885bade356
yymin1022 <yymin1022@gmail.com>
2021-04-17 16:12:20 +0900

    Update Webpack Config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,34 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	mode: "production",
-	entry: "./src/index.js",
-	output: {
-		path: path.resolve("build"),
-		filename: "index.js",
-		libraryTarget: "commonjs2"
-	},
-	module: {
-		rules: [
-			{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-			{
-				test: /\.css$/,
-				loader: "style-loader!css-loader"
-			}
-		]
-	},
-	externals: {
-		react: "react"
-	}
-};
+    mode: "development",
+    entry: path.resolve(__dirname, "src/index.js"),
+    output: {
+        path: path.resolve(__dirname, "output"),
+        filename: "bundle.js"
+    },
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
+    module: {
+        rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+            filename: "./index.html"
+         })
+    ]
+}
