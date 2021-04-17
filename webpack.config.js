2138eebd11be90f76d987ce917fa76394a364dbf
yymin1022 <yymin1022@gmail.com>
2021-04-17 16:19:44 +0900

    Update Webpack Config : Add Markdown Rules

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,11 @@ module.exports = {
             },
             {
                 test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,loader: "url-loader?limit=100000"
-            }
+            },
+            {
+                test: /\.md$/, 
+                use: ['markdown-loader', 'html-loader']
+            },
         ]
     },
     plugins: [
