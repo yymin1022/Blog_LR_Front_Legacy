47500795d6483a7409040ff00bfd7c2716ee1bb9
yymin1022 <yymin1022@gmail.com>
2021-04-19 20:15:23 +0900

    Update Webpack Config

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
                 use: ["style-loader", "css-loader"],
             },
             {
-                test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,loader: "url-loader?limit=100000"
+                test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|otf|svg)$/,loader: "url-loader?limit=100000"
             },
             {
                 test: /\.md$/, 
