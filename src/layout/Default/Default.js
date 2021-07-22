525bfd84d8552a1cbffc41566c7fef1a77edfa0e
yymin1022 <yymin1022@gmail.com>
2021-07-22 11:02:44 +0000

    Renewed Default Layout CSS

--- src/layout/Default/Default.js
+++ src/layout/Default/Default.js
@@ -10,8 +10,10 @@ import clsx from "clsx";
 import List from "@material-ui/core/List";
 import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
 
-import styled, {createGlobalStyle} from "styled-components";
+import {createGlobalStyle} from "styled-components";
 import {makeStyles} from "@material-ui/core/styles";
+
+import "./Default.css"
 
 const GlobalStyle = createGlobalStyle`
   html, body {
@@ -35,29 +37,6 @@ const drawerStyle = makeStyles({
  },
 });
 
-const Container = styled.div`
-  padding-top: 100px;
-  padding-bottom: 150px;
-  padding-left: 50px;
-  padding-right: 50px;
-
-  height: 100%;
-
-  /* Mobile UI */
-  @media screen and (max-width: 768px){
-    padding-top: 85px;
-    padding-left: 10px;
-    padding-right: 10px;
-  }
-`;
-
-const NavContainer = styled.div`
-  align-items: center;
-  display: flex;
-  margin-bottom: -10px;
-  margin-left: 10px;
-  margin-top: -10px;
-`;
 
 const Default = props => {
   const {
@@ -104,43 +83,43 @@ const Default = props => {
           state:{
             categoryID: "all"
           }}}>
-          <NavContainer>
+          <div id="NavContainer">
             <p style={{color: "#333333"}}>All Posts</p>
-          </NavContainer>
+          </div>
         </Link>
         <Link to={{
           pathname: "/",
           state:{
             categoryID: "dev"
           }}}>
-          <NavContainer>
+          <div id="NavContainer">
             <p style={{color: "#333333"}}>Development</p>
-          </NavContainer>
+          </div>
         </Link>
         <Link to={{
           pathname: "/",
           state:{
             categoryID: "os"
           }}}>
-          <NavContainer>
+          <div id="NavContainer">
             <p style={{color: "#333333"}}>Operating System</p>
-          </NavContainer>
+          </div>
         </Link>
         <Link to={{
           pathname: "/",
           state:{
             categoryID: "synology"
           }}}>
-          <NavContainer>
+          <div id="NavContainer">
             <p style={{color: "#333333"}}>Synology/XPEnology</p>
-          </NavContainer>
+          </div>
         </Link>
       </List>
     </div>
   );
   
   return (
-    <Container className={className}>
+    <div id="Container" className={className}>
       <GlobalStyle />
       <Header btnAction={toggleDrawer(anchor, true)}/>
       <SwipeableDrawer
@@ -153,7 +132,7 @@ const Default = props => {
       <Section>
         {children}
       </Section>
-    </Container>
+    </div>
   );
 }
 
