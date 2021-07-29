076b9006fd63846ec65e95e7313e064696eb7f10
yymin1022 <yymin1022@gmail.com>
2021-07-29 10:14:42 +0000

    Update SideMenu.js : Improve Elements Structure

--- src/layout/Default/SideMenu/SideMenu.js
+++ src/layout/Default/SideMenu/SideMenu.js
@@ -1,11 +1,9 @@
-import React,  {useEffect, useState} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
-import {makeStyles} from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
 import MenuIcon from "@material-ui/icons/Menu";
 
 import "./SideMenu.css";
@@ -24,9 +22,9 @@ const SideMenu = (props) => {
                 <IconButton onClick={btnAction} edge="start" className="SideMenuButton" color="inherit" aria-label="menu">
                     <MenuIcon />
                 </IconButton>
-                <Typography variant="h6" className="SideMenuTitle">
+                <h6 variant="h6" className="SideMenuTitle">
                     LR의 IT블로그
-                </Typography>
+                </h6>
             </div>
 
             <div id="SideMenuBottom">
