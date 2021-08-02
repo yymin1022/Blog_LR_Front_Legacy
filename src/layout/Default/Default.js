c0f4fd94802b63e05205d93419f370054ad4d720
yymin1022 <yymin1022@gmail.com>
2021-08-02 07:04:00 +0000

    Update Files : Setup Imports

--- src/layout/Default/Default.js
+++ src/layout/Default/Default.js
@@ -1,21 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import {makeStyles} from "@material-ui/core/styles";
-
 import SideMenu from "./SideMenu";
 import Section from "./Section";
+
 import "./Default.css"
-
-const drawerStyle = makeStyles({
-    list: {
-        width: 250,
-    },
-    fullList: {
-        width: "auto",
-    },
-});
-
 
 const Default = (props) => {
     const {
