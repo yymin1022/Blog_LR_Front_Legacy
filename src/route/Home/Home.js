b1cf86815e1fd44480c60ce6af105473c6956641
yymin1022 <yymin1022@gmail.com>
2021-08-02 10:19:34 +0000

    Update Home.css : Improve Element Structure

--- src/route/Home/Home.js
+++ src/route/Home/Home.js
@@ -9,9 +9,9 @@ const Home = () => {
     return(
         <div>
             <div id="divTitle">
-                <p id="blogTitle1">1인개발자</p>
-                <p id="blogTitle2">LR의</p>
-                <p id="blogTitle3">IT블로그</p>
+                <p className="blogTitle" id="blogTitle1">1인개발자</p>
+                <p className="blogTitle" id="blogTitle2">LR의</p>
+                <p className="blogTitle" id="blogTitle3">IT블로그</p>
             </div>
 
             <div id="divNav">
