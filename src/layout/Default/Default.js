c7f310e12786832f6323905c499c4b50d7a1650b
yymin1022 <yymin1022@gmail.com>
2021-08-02 06:59:45 +0000

    Update Files : Setup Imports

--- src/layout/Default/Default.js
+++ src/layout/Default/Default.js
@@ -1,11 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import SideMenu from "./SideMenu";
-import Section from "./Section";
-
 import {makeStyles} from "@material-ui/core/styles";
 
+import SideMenu from "./SideMenu";
+import Section from "./Section";
 import "./Default.css"
 
 const drawerStyle = makeStyles({
--- src/layout/Default/SideMenu/SideMenu.js
+++ src/layout/Default/SideMenu/SideMenu.js
@@ -3,8 +3,6 @@ import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 
 import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 
 import "./SideMenu.css";
 
