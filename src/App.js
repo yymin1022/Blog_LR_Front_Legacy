79bb573580bbe0b19e02fc54872a26e017f144b1
yymin1022 <yymin1022@gmail.com>
2021-08-02 06:27:08 +0000

    Update App.js : Apply New Depth Structure

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import RouteLayout from "./layout/RouteLayout";
 import DefaultLayout from "./layout/Default";
 
 import About from "./route/About";
+import Blog from "./route/Blog";
 import Home from "./route/Home";
 import NotFoundPage from "./route/NotFoundPage";
 import PostView from "./route/PostView";
+import Project from "./route/Project";
 import ScrollTop from "./ScrollTop";
 
 const App = () => {
@@ -20,6 +22,16 @@ const App = () => {
                         path = "/"
                         layout = {DefaultLayout}
                         component = {withRouter(Home)} />
+                    <RouteLayout
+                        exact
+                        path = "/blog"
+                        layout = {DefaultLayout}
+                        component = {withRouter(Blog)} />
+                    <RouteLayout
+                        exact
+                        path = "/project"
+                        layout = {DefaultLayout}
+                        component = {withRouter(Project)} />
                     <RouteLayout
                         exact
                         path = "/about"
