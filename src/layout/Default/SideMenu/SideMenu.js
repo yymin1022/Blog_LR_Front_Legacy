2e8539f8b862aaeb98ac0ee2d00eab9c66f45717
yymin1022 <yymin1022@gmail.com>
2021-08-02 08:46:38 +0000

    Update SideMenu.js : Removed Unused Imports

--- src/layout/Default/SideMenu/SideMenu.js
+++ src/layout/Default/SideMenu/SideMenu.js
@@ -1,8 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {Link} from "react-router-dom";
-
-import Button from "@material-ui/core/Button";
 
 import "./SideMenu.css";
 
