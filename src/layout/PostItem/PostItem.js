967b351fcee924a09e58c750abaa9738873a6701
yymin1022 <yymin1022@gmail.com>
2021-08-28 15:26:03 +0900

    Update PostItem.js : Add Pinned Post Filtering

--- src/layout/PostItem/PostItem.js
+++ src/layout/PostItem/PostItem.js
@@ -1,11 +1,23 @@
 import React from "react";
 
 const PostItem = (props) => {
-    return(
-        <div className="postItem" id={props.postURL}>
-            <p>{props.postTitle}</p>
-        </div>
-    )
+    let isPinned = props.pinned;
+    let postDate = props.postDate;
+    let postTag = props.postTag;
+    let postTitle = props.postTitle;
+    let postURL = props.postURL;
+
+    if(isPinned){
+            <div className="postItemPinned" id={postURL}>
+                <p>!!Pinned!! {postTitle}</p>
+            </div>
+    }else{
+        return(
+            <div className="postItem" id={postURL}>
+                <p>{postTitle}</p>
+            </div>
+        )
+    }
 }
 
 export default PostItem;
