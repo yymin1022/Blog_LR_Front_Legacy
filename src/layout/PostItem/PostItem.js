8ad71568ea292ae8d6ab7beef99f1d60def70bd9
yymin1022 <yymin1022@gmail.com>
2021-08-28 14:57:42 +0900

    Add PostItem Component

---
+++ src/layout/PostItem/PostItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PostItem = (props) => {
+    return(
+        <div id="postItem">
+            <p>{props.postTitle}</p>
+        </div>
+    )
+}
+
+export default PostItem;
