6e6d20b0e898bd9ed828294aaa9cb69add1d4cf4
yymin1022 <yymin1022@gmail.com>
2021-09-10 20:09:54 +0900

    Update PostRenderer : Debug Image Render

--- src/route/PostView/PostRenderer.js
+++ src/route/PostView/PostRenderer.js
@@ -4,7 +4,7 @@ const PostRenderer = (postID) => {
     return {
         img: ({src, width, ...props}) => (
             <>
-                {console.log(postID)}
+                {console.log(src)}
                 <img src={require(`../../postData/${postID}/${src}`).default} width={width} {...props} />
             </>
         )
