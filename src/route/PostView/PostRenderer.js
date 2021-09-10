820b02a958b400929a2cfa2f2f9f0b46108b3b27
yymin1022 <yymin1022@gmail.com>
2021-09-10 19:35:38 +0900

    Update PostRenderer : Debug Image Render

--- src/route/PostView/PostRenderer.js
+++ src/route/PostView/PostRenderer.js
@@ -2,10 +2,9 @@ import React from "react";
 
 const PostRenderer = (postID) => {
     return {
-        img: ({src, width, ...props}) =>
-            <>
-                <img src={require(`../../postData/${postID}/${src}`).default} width={width} {...props} />
-            </>
+        img: ({src, width, ...props}) => (
+            <img src={require(`../../postData/${postID}/${src}`).default} width={width} {...props} />
+        )
     }
 }
 
