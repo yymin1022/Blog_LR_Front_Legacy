8b93adab70d44dab0f707482c95256165a36e894
yymin1022 <yymin1022@gmail.com>
2021-09-10 20:59:26 +0900

    Update PostRenderer : Debug Image Render

--- src/route/PostView/PostRenderer.js
+++ src/route/PostView/PostRenderer.js
@@ -4,7 +4,10 @@ const PostRenderer = (postID) => {
     return {
         img: ({src, width, ...props}) =>
             <>
-                {console.log(require(`../../postData/${postID}/${src}`).default)}
+                {
+                    console.log(require(`../../postData/${postID}/${src}`).default),
+                    console.log(require("../../postData/210202-synology-docker-vscode-install/0_vscode_usage.png").default)
+                }
                 <img src={require(`../../postData/${postID}/${src}`).default} width={width} {...props} />
             </>
     }
