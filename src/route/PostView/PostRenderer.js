67181dfb4587b4d5abad123f7008df50fb819223
yymin1022 <yymin1022@gmail.com>
2021-09-10 21:08:51 +0900

    Update PostRenderer : Remove Debug Code

--- src/route/PostView/PostRenderer.js
+++ src/route/PostView/PostRenderer.js
@@ -1,12 +1,11 @@
 import React from "react";
 
+import testImg from "../../postData/210202-synology-docker-vscode-install/0_vscode_usage.png";
+
 const PostRenderer = (postID) => {
     return {
         img: ({src, width, ...props}) =>
             <>
-                {
-                    console.log(require(`../../postData/${postID}/${src}`))
-                }
                 <img src={require(`../../postData/${postID}/${src}`)} width={width} {...props} />
             </>
     }
