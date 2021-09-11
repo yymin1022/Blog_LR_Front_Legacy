e8bed87c59ae9039cf65694e5131b2c13573bbe4
yymin1022 <yymin1022@gmail.com>
2021-09-11 20:47:14 +0900

    Update PostList.js : Add Debug Code

--- src/route/PostList/PostList.js
+++ src/route/PostList/PostList.js
@@ -13,13 +13,16 @@ import "./PostList.css";
 const PostList = (props) => {
     let curPath = window.location.pathname;
     let postDB = blogDB;
+    let postType = "blog";
 
     console.log(curPath);
 
     if(curPath === "/blog"){
         postDB = blogDB;
+        postType = "blog";
     }else if(curPath === "/project"){
         postDB = projectDB;
+        postType = "project";
     }
 
     return(
