8b25090365714b06651a98d242d6e2ee656bc185
yymin1022 <yymin1022@gmail.com>
2021-09-11 21:18:52 +0900

    Update PostList.js : Cleaned UP

--- src/route/PostList/PostList.js
+++ src/route/PostList/PostList.js
@@ -1,5 +1,4 @@
 import React from "react";
-import {If, Then} from "react-if";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faThumbtack} from "@fortawesome/free-solid-svg-icons"
 
