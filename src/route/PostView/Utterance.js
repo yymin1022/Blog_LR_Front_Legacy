22f20644e8f4bf479ffd86c9187221f911bedb24
yymin1022 <yymin1022@gmail.com>
2021-09-11 12:52:15 +0900

    Update Utterance.js : Add Basic Code

--- src/route/PostView/Utterance.js
+++ src/route/PostView/Utterance.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {createRef, useEffect} from "react";
 
 const Utterance = () => {
+    const commentRef = createRef();
+
+    useEffect(() => {
+        const utteranceElement = document.createElement("script");
+        const utteranceConfig = {
+            async: true,
+            crossorigin: "anonymus",
+            repo: "yymin1022/Blog_LR_Comments",
+            src: "https://utteranc.es/client.js",
+            theme: "github-light",
+            "issue-term": "url"
+        };
+
+        Object.entries(utteranceConfig).forEach(([key, value]) => {
+            utteranceElement.setAttribute(key, value);
+        });
+
+        commentRef.current.appendChile(utteranceElement);
+    }, []);
+
     return(
-        <div>
-            Utterance Component
-        </div>
+        <div className="divComment" ref={commentRef}></div>
     )
 }
 
