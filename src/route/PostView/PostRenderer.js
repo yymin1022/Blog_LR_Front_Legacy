0fd858c174c41409ff1b35d521917c695013fcfe
yymin1022 <yymin1022@gmail.com>
2021-09-12 10:52:20 +0900

    Update PostRenderer.js : Apply Prism Highlight on Code

--- src/route/PostView/PostRenderer.js
+++ src/route/PostView/PostRenderer.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 import {Prism as SyntaxHighlighter} from "react-syntax-highlighter";
+import {darcula} from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 const PostRenderer = (postID, postType) => {
     return {
         code: ({inline, className, children, ...props}) => {
             const match = /language-(\w+)/.exec(className || '');
             return !inline && match 
-                ? <SyntaxHighlighter style={okaidia} language={match[1]} children={String(children).replace(/\n$/, '')} {...props}/>
+                ? <SyntaxHighlighter style={darcula} language={match[1]} children={String(children).replace(/\n$/, '')} {...props}/>
                 : <code className={className} {...props}/>;
         },
 
