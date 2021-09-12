bd9b144078b6cc91bfac1eb9919bd17bee6534a4
yymin1022 <yymin1022@gmail.com>
2021-09-12 20:01:05 +0900

    Update About : Fix Component

--- src/route/About/About.js
+++ src/route/About/About.js
@@ -2,8 +2,9 @@ import React, {useEffect, useState} from "react";
 
 import MDFile from "../../postData/about.md";
 import ReactMarkdown from "react-markdown";
+import RehypeRaw from "rehype-raw";
+import RemarkGFM from "remark-gfm";
 
-import CodeBlock from "./CodeBlock";
 import "./About.css";
 
 const About = () => {
@@ -20,138 +21,24 @@ const About = () => {
             window.scrollTop = 0;
         }
     }, []);
-    const BlockQuoteBlock = (props) => {
-        return (
-            <div className="quoteBlock" style={{}}>
-                {props.children}
-            </div>
-        );
-    }
-
-    const InlineCodeBlock = (props) => {
-        return(
-            <span className="inlineCodeBlock">
-                {props.value}
-            </span>
-        )
-    }
-    
-    const TableCellBlock = (props) => {
-        let style ={
-            textAlign: props.align ? props.align : "center",
-            padding: 5
-        };
-    
-        if (props.isHeader){
-            style.background = "#ffff00";
-            style.border = "1px solid #cccccc";
-            style.borderLeft = 0;
-            style.borderRight = 0;
-        }else{
-            style.borderBottom = "1px solid #eeeeee";
-        }
-    
-        return(
-            <td style={style}>
-                {props.children}
-            </td>
-        );
-    }
 
     return(
         <div align="center">
             <div className="AboutContent">
                 <div className="markdown-body-about">
                     <ReactMarkdown
-                        source={postData}
-
-                        allowDangerousHtml={true}
-                        escapeHtml={false}
-                        skipHtml={false}
-                        renderers={{
-                            blockquote: BlockQuoteBlock,
-                            code: CodeBlock,
-                            inlineCode: InlineCodeBlock,
-                            tableCell: TableCellBlock
-                        }} />
+                        children={postData}
+                        components={PostRenderer(postID, postType)}
+                        rehypePlugins={[RehypeRaw]}
+                        remarkPlugins={[RemarkGFM]} />
                 </div>
+            </div>
+
+            <div className="divAboutComment">
+                <Utterance />
             </div>
         </div>
     )
 }
-
-// class About extends Component {
-//     constructor() {
-//         super();
-//         this.state = { markdown: "" };
-//     }
-
-//     UNSAFE_componentWillMount() {
-//         fetch(MDFile).then(res => res.text()).then(text => this.setState({ markdown: text }));
-//     }
-
-//     render() {
-//         function BlockQuoteBlock(props) {
-//             return (
-//                 <div className="quoteBlock" style={{}}>
-//                     {props.children}
-//                 </div>
-//             );
-//         }
-
-//         function InlineCodeBlock(props){
-//             return(
-//                 <span className="inlineCodeBlock">
-//                     {props.value}
-//                 </span>
-//             )
-//         }
-        
-//         function TableCellBlock(props) {
-//             let style = {
-//                 textAlign: props.align ? props.align : "center",
-//                 padding: 5
-//             };
-        
-//             if (props.isHeader) {
-//                 style.background = "#ffff00";
-//                 style.border = "1px solid #cccccc";
-//                 style.borderLeft = 0;
-//                 style.borderRight = 0;
-//             }else{
-//                 style.borderBottom = "1px solid #eeeeee";
-//             }
-        
-//             return (
-//                 <td style={style}>
-//                     {props.children}
-//                 </td>
-//             );
-//         }
-
-//         const {markdown} = this.state;
-
-//         return(
-//             <div align="center">
-//                 <div className="AboutContent">
-//                     <div className="markdown-body-about">
-//                         <ReactMarkdown
-//                             source={markdown}
-
-//                             allowDangerousHtml={true}
-//                             escapeHtml={false}
-//                             skipHtml={false}
-//                             renderers={{
-//                                 blockquote: BlockQuoteBlock,
-//                                 code: CodeBlock,
-//                                 inlineCode: InlineCodeBlock,
-//                                 tableCell: TableCellBlock
-//                           }} />
-//                     </div>
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
 
 export default About;
