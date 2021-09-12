12a1faf2b61c1ca38cd061a77b2720181df8f96f
yymin1022 <yymin1022@gmail.com>
2021-09-12 10:45:26 +0900

    Update PostRenderer.js : Apply Prism Highlight on Code Render

--- src/route/PostView/PostRenderer.js
+++ src/route/PostView/PostRenderer.js
@@ -1,11 +1,21 @@
 import React from "react";
 
+import {Prism as SyntaxHighlighter} from "react-syntax-highlighter";
+
 const PostRenderer = (postID, postType) => {
     return {
-        img: ({src, width, ...props}) =>
+        code: ({inline, className, children, ...props}) => {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match 
+                ? <SyntaxHighlighter style={okaidia} language={match[1]} children={String(children).replace(/\n$/, '')} {...props}/>
+                : <code className={className} {...props}/>;
+        },
+
+        img: ({src, width, ...props}) => {
             <>
                 <img src={require(`../../postData/${postType}/${postID}/${src}`)} width={width} {...props} />
             </>
+        }
     }
 }
 
