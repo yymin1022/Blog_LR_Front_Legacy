fcc781e5bb1d8bc42eb270734cc789b3f3ad0abd
yymin1022 <yymin1022@gmail.com>
2021-09-26 17:17:17 +0900

    Update Meta Info per Page

--- src/Route/Home/Home.js
+++ src/Route/Home/Home.js
@@ -4,6 +4,8 @@ import {Link} from "react-router-dom";
 
 import Button from "@material-ui/core/Button";
 
+import Metadata from "../../Util/Metadata.js";
+
 import "./Home.css";
 
 const Home = () => {
@@ -15,12 +17,7 @@ const Home = () => {
 
     return(
         <>
-            <Helmet>
-                <title>LR's IT Blog</title>
-                <meta name="description" property="og:description" content="1인개발자 LR의 IT블로그" />
-                <meta name="image" property="og:image" content="logo.png" />
-                <meta name="url" property="og:url" content={window.location.href} />
-            </Helmet>
+            <Metadata data={title: "LR's IT Blog"} />
 
             <div className="divHome">
                 <div id="divTitle">
--- src/Util/Metadata.js
+++ src/Util/Metadata.js
@@ -3,10 +3,7 @@ import {Helmet} from 'react-helmet';
 
 const Metadata = ({data}) => {
     const title = data.title;
-    const description = data.description;
     const image = data.image !== undefined && `${data.image}`;
-    const width = data.image && (data.width || 500);
-    const height = data.image && (data.height || 500);
 
     return (
         <Helmet>
@@ -14,9 +11,9 @@ const Metadata = ({data}) => {
             <meta property="og:title" content={title} />
             <meta property="og:description" content="대학생 1인개발자 LR의 IT블로그" />
 
-            {image ? <meta property="og:image" content={image} /> : <meta property="og:image" content={image}/>}
-            {width ? <meta property="og:image:width" content={width} /> : null}
-            {height ? <meta property="og:image:height" content={height} /> : null}
+            {image ? <meta property="og:image" content={image} /> : <meta property="og:image" content={"logo.png"}/>}
+            <meta property="og:image:width" content={500} />
+            <meta property="og:image:height" content={500} />
         </Helmet>
     );
 };
