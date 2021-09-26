3191be4618b1616f94d54036b23bc87e71b27ce6
yymin1022 <yymin1022@gmail.com>
2021-09-26 12:45:42 +0900

    Update Meta Info per Page

--- src/Route/PostList/PostList.js
+++ src/Route/PostList/PostList.js
@@ -12,86 +12,79 @@ import "./PostList.css";
 
 const PostList = () => {
     let curPath = window.location.pathname;
+    let pageType = (curPath == "/blog") ? "Blog" : "Projects";
     let postDB = (curPath == "/blog") ? blogDB : projectDB;
     let postType = (curPath == "/blog") ? "blog" : "project";
 
     useEffect(() => {
-        let pageType = (curPath == "/blog") ? "Blog" : "Projects";
-        setMetaTags({title: `${pageType} - LR's IT Blog`});
-
         return () => {
             window.scrollTop = 0;
         }
     }, []);
 
     return(
-        <div align="center" className="postListContainer">
-            <div align="left" className="postPinnedTitle">
-                <FontAwesomeIcon icon={faThumbtack} color="#606060" size="lg"/>
-                <a style={{color: "#606060", marginLeft: "5px", textSize: "25px"}}>
-                    Pinned
-                </a>
+        <>
+            <Helmet meta={[
+                {property: "og:title", content: `${pageType} - LR's IT Blog` },
+                {property: "og:description", content: "대학생 1인개발자 LR의 IT블로그" },
+                {property: "og:image", content: "%PUBLIC_URL%/logo.png" },
+            ]} />
+            <div align="center" className="postListContainer">
+                <div align="left" className="postPinnedTitle">
+                    <FontAwesomeIcon icon={faThumbtack} color="#606060" size="lg"/>
+                    <a style={{color: "#606060", marginLeft: "5px", textSize: "25px"}}>
+                        Pinned
+                    </a>
+                    <br/>
+                </div>
+                
+                <div className="postPinnedContainer">
+                    {
+                        postDB.map(item => {
+                            if(item.pinned){
+                                return(
+                                    <PostItem
+                                        pinned={item.pinned}
+                                        postDate={item.postDate}
+                                        postTag={item.postTag}
+                                        postTitle={item.postTitle}
+                                        postType={postType}
+                                        postURL={item.postURL} />
+                                );
+                            }
+                        })
+                    }
+                </div>
+
                 <br/>
-            </div>
-            
-            <div className="postPinnedContainer">
-                {
-                    postDB.map(item => {
-                        if(item.pinned){
-                            return(
-                                <PostItem
-                                    pinned={item.pinned}
-                                    postDate={item.postDate}
-                                    postTag={item.postTag}
-                                    postTitle={item.postTitle}
-                                    postType={postType}
-                                    postURL={item.postURL} />
-                            );
-                        }
-                    })
-                }
-            </div>
 
-            <br/>
+                <div className="PostAd" id="CaulyAdvertisement">
+                    <CaulyView />
+                </div>
 
-            <div className="PostAd" id="CaulyAdvertisement">
-                <CaulyView />
+                <hr className="seperator"/>
+                <br/>
+
+                <div className="postContainer">
+                    {
+                        postDB.map(item => {
+                            if(!item.pinned){
+                                return(
+                                    <PostItem
+                                        pinned={item.pinned}
+                                        postDate={item.postDate}
+                                        postTag={item.postTag}
+                                        postTitle={item.postTitle}
+                                        postType={postType}
+                                        postURL={item.postURL} />
+                                );
+                            }
+                        })
+                    }
+                </div>
             </div>
-
-            <hr className="seperator"/>
-            <br/>
-
-            <div className="postContainer">
-                {
-                    postDB.map(item => {
-                        if(!item.pinned){
-                            return(
-                                <PostItem
-                                    pinned={item.pinned}
-                                    postDate={item.postDate}
-                                    postTag={item.postTag}
-                                    postTitle={item.postTitle}
-                                    postType={postType}
-                                    postURL={item.postURL} />
-                            );
-                        }
-                    })
-                }
-            </div>
-        </div>
+        </>
     )
 }
-
-const setMetaTags = (
-    {
-        title = "LR's IT Blog",
-        description = "대학생 1인개발자 LR의 IT블로그",
-        imageUrl = "logo.png"
-    }) => {
-    document.querySelector('meta[property="og:title"]').setAttribute("content", `${title}`);
-    document.querySelector('meta[property="og:description"]').setAttribute("content", description);
-    document.querySelector('meta[property="og:image"]') .setAttribute("content", imageUrl);
-    document.querySelector('meta[property="og:url"]') .setAttribute("content", window.location.href);
-};
 
 export default PostList;
