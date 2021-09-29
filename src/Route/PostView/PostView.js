51072b27c6ebc85bc3435ce8a916932a3efc0727
yymin1022 <yymin1022@gmail.com>
2021-09-29 16:36:18 +0900

    Update PostView.js : Add Comment about Posts

--- src/Route/PostView/PostView.js
+++ src/Route/PostView/PostView.js
@@ -14,6 +14,12 @@ import projectDB from "../../Post/project/DB.json";
 
 import "./PostView.css";
 
+/********
+Blog : Pinned 4개. 
+Projects : Pinned 4개.
+Problem Solving : Pinned 없음. Date에 문제번호 기록 ex) BOJ 1022 / Programmers 1022
+*********/
+
 const PostView = (props) => {
     const [postData, setPostData] = useState("");
     const [postDate, setPostDate] = useState("");
